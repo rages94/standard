f3fee7ce149387f4ba41a68bf852337ae0b5ace2
dprokazin <prda@liis.su>
2025-06-01 14:22:39 +0300

    fix: commands

--- src/worker/commands.py
+++ src/worker/commands.py
@@ -1,3 +1,9 @@
+import sys, os
+
+sys.path.append(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+)
+
 import asyncio
 
 from src.containers.container import container
